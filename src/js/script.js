6849a13acb370a02118160271bacd2388e96026e
Fernanda Borges <89820803+fernandasborges@users.noreply.github.com>
2021-11-04 19:37:10 -0300

    arquivo javascript

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+const btnMenu = document.getElementById('btn-menu');
+
+function toggleMenu(event){
+    if (event.type === 'touchstart') event.preventDefault();
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+    const active = nav.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded', active);
+    if(active){   
+        event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+    } else{
+        event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+    }
+}
+btnMenu.addEventListener('click', toggleMenu);
+btnMenu.addEventListener('touchstart', toggleMenu);
+
