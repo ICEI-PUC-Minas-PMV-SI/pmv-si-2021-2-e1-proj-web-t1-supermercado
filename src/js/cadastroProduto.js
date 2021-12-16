fb6b666410972e49eff1c92beac6c4cdb4d3e734
Fernanda Borges <89820803+fernandasborges@users.noreply.github.com>
2021-12-15 21:23:29 -0300

    js cadastro produto

---
+++ src/js/cadastroProduto.js
@@ -0,0 +1,132 @@
+// URL DA API DE DADOS
+URL = 'http://localhost:3000/produtos' 
+
+//=================================================================================================
+// GET - Recupera todos os produtos e adiciona na tabela
+
+const produtoList = document.getElementById('produto-list');
+
+fetch(URL)
+    .then(res => res.json())
+    .then(produtos => {
+        let lista_produtos = '';
+        for (let i = 0; i < produtos.length; i++) {
+            lista_produtos += `
+            <tr>
+                <th>${produtos[i].id}</th>
+                <th>${produtos[i].categoria}</th>
+                <td><img src="${produtos[i].imagem}" width="60px" height="60px"></td>
+                <td><img src="${produtos[i].logoS}" width="30px" height="30px"> </td>           
+                <td>${produtos[i].nome}</td>
+                <td>${produtos[i].descricao}</td>
+                <td>R$${(parseFloat(produtos[i].vlr)).toFixed(2)}</td>
+                <td>
+                    <a onclick="getProduto(${produtos[i].id});" 
+                    class="btn btn-warning btn-sm" 
+                    data-toggle="modal" data-target="#produto-modal">
+                    <i class="fa fa-edit"></i>  Editar
+                    </a>
+                    <a onclick="$('#id-prod').text(${produtos[i].id});" class="btn btn-danger btn-sm" 
+                    data-toggle="modal" data-target="#modal-delete">
+                    <i class="fa fa-trash"></i> Remover
+                    </a>
+                </td>
+            </tr>
+            `;
+            produtoList.innerHTML = lista_produtos;
+        }
+    });
+//=================================================================================================
+
+// DELETE - PROCEDIMENTO PARA EXCLUIR UM PRODUTO
+const produtoDelete = document.getElementById('btn-delete');
+
+produtoDelete.addEventListener('click', (e) => {
+
+    let id = $('#id-prod').text();
+
+    fetch(`${URL}/${id}`, {
+        method: 'DELETE',
+    })
+    .then(res => res.json())
+    .then(() => location.reload());
+
+})
+//=================================================================================================
+
+// PROCEDIMENTO PARA RECUPERAR OS DADOS DE UM PRODUTO NA API
+function getProduto(id){
+
+    if(id == 0){
+        $('#edit-prod-id').text("");
+        $( "#produto-id" ).prop( "disabled", false );
+        $('#produto-id').val("");
+        $('#produto-categoria').val("");
+        $('#produto-imagem').val("");
+        $('#produto-logoS').val("");
+        $('#produto-nome').val("");
+        $('#produto-descricao').val("");
+        $('#produto-vlr').val("");
+    }else{
+        $('#edit-prod-id').text(id);
+        fetch(`${URL}/${id}`).then(res => res.json())    
+        .then(data => {
+            $( "#produto-id" ).prop( "disabled", true );
+            $('#produto-id').val(data.id);
+            $('#produto-categoria').val(data.categoria);
+            $('#produto-imagem').val(data.imagem);
+            $('#produto-logoS').val(data.logoS);
+            $('#produto-nome').val(data.nome);
+            $('#produto-descricao').val(data.descricao);
+            $('#produto-vlr').val(data.vlr);
+        });
+    }    
+}
+
+//=================================================================================================
+
+// CREATE or UPDATE - PROCEDIMENTO PARA CRIAR OU EDITAR UM PRODUTO
+
+const produtoForm = document.getElementById('produto-form');
+
+produtoForm.addEventListener('submit', (e) => {
+
+    // RECUPERA O ID DO PRODUTO
+    let id = parseInt($('#edit-prod-id').text());    
+
+    // RECUPERA OS DADOS DO PRODUTO
+    const produto = JSON.stringify({
+        id: document.getElementById('produto-id').value,
+        categoria: document.getElementById('produto-categoria').value,
+        imagem: document.getElementById('produto-imagem').value,
+        logoS: document.getElementById('produto-logoS').value,
+        nome: document.getElementById('produto-nome').value,
+        descricao: document.getElementById('produto-descricao').value,
+        vlr: document.getElementById('produto-vlr').value,
+    })
+
+    if (id >= 0) {
+        fetch(`${URL}/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: produto
+        })
+        .then(res => res.json())
+        .then(() => location.reload());  
+    }
+    else{ 
+        fetch(URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: produto
+        })
+        .then(res => res.json())
+        .then(() => location.reload());  
+    }      
+})
+
+
